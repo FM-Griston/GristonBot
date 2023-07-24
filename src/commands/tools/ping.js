086824c2ee0e37fbac197051893e65ccea992228
Green <71292700+TEH-Green@users.noreply.github.com>
2023-07-24 17:28:44 +0200

    1.0.0

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Mi a bot pingje?'),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+
+        const newMessage = `API Késleltetése: ${client.ws.ping}\nBot Pingje: ${message.createdTimestamp - interaction.createdTimestamp}`
+        await interaction.editReply({
+            content: newMessage
+        });
+    },
+};
